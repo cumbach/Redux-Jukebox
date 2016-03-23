8bf93d986e9b9af3a467882b8e795218e79f2397
Frank Gutierrez <dearfrankg@gmail.com>
2016-03-22 22:54:06 -0700

    cleanup actions

--- app/actions/index.js
+++ app/actions/index.js
@@ -12,20 +12,22 @@ function action(type, payload = {}) {
   return {type, ...payload}
 }
 
-
+////////////////////////////////////////////////////
+// CREATE CONSTANTS
+//
 export const LOAD_SONG_BY_ARTIST = 'LOAD_SONG_BY_ARTIST'
 export const LOAD_SONG_BY_NAME = 'LOAD_SONG_BY_NAME'
 export const RESET_SCREEN = 'RESET_SCREEN'
-
-////////////////////////////////////////////////////
-// Create structure of constant names
-//
 export const SONG_BY_ARTIST = createRequestTypes('SONG_BY_ARTIST')
 export const SONG_BY_NAME = createRequestTypes('SONG_BY_NAME')
 
 ////////////////////////////////////////////////////
-// Create action creators for each constant
+// CREATE ACTION CREATORS
 //
+export const loadSongByArtist = (artistName) => action(LOAD_SONG_BY_ARTIST, {artistName})
+export const loadSongByName = (songName) => action(LOAD_SONG_BY_NAME, {songName})
+export const resetScreen = () => action(RESET_SCREEN)
+
 export const songByArtist = {
   request: name => action(SONG_BY_ARTIST.REQUEST, {name}),
   success: (name, response) => action(SONG_BY_ARTIST.SUCCESS, {name, response}),
@@ -37,7 +39,3 @@ export const songByName = {
   success: (name, response) => action(SONG_BY_NAME.SUCCESS, {name, response}),
   failure: (name, error) => action(SONG_BY_NAME.FAILURE, {name, error}),
 }
-
-export const resetScreen = () => action(RESET_SCREEN)
-export const loadSongByArtist = (artistName) => action(LOAD_SONG_BY_ARTIST, {artistName})
-export const loadSongByName = (songName) => action(LOAD_SONG_BY_NAME, {songName})
